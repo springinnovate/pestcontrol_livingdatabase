088fc4ff8507a1ddec8eae454fbfb8144f3dec5e
Rich Sharp <rich@therealspring.org>
2021-12-06 09:51:14 -0800

    printing graph along with table

--- clean_table.py
+++ clean_table.py
@@ -13,30 +13,36 @@ TABLE_PATH = 'plotdata.cotton.select.csv'
 CLUSTER_RESOLUTION = 0.01
 
 
-def main():
-    """Entry point."""
-    table = pandas.read_csv(
-        TABLE_PATH, encoding='unicode_escape', engine='python')
-    X = table[['long', 'lat']]
-    print(X['long'].min(), X['long'].max())
-    fig, ax = plt.subplots()
-    brc = Birch(threshold=0.01, n_clusters=None)
-    brc.fit(X.values)
-    clusters = brc.predict(X.values)
-    table['clusters'] = clusters
-
-    #table.plot.scatter(y='lat', x='long', s=0.5)
+def _generate_scatter_plot(clusters, table):
+    """Generate scatter plot of clusters."""
     print('generating scatter plot')
+    fig, ax = plt.subplots()
     colorlist = list(colors.ColorConverter.colors.keys())
     for i, cluster in enumerate(numpy.unique(clusters)):
         df = table[table['clusters'] == cluster]
         df.plot.scatter(
             x='long', y='lat', ax=ax, s=0.01, marker='x',
             color=colorlist[i % len(colorlist)])
-        ax.set_title(f'{os.path.basename(TABLE_PATH)} {len(clusters)} clusters\nwithin ${CLUSTER_RESOLUTION}^\\circ$ of each other')
+        ax.set_title(
+            f'{os.path.basename(TABLE_PATH)} {len(clusters)} clusters\n'
+            f'within ${CLUSTER_RESOLUTION}^\\circ$ of each other')
     plt.savefig(
         f'{os.path.basename(os.path.splitext(TABLE_PATH)[0])}.png', dpi=600)
 
+
+def main():
+    """Entry point."""
+    table = pandas.read_csv(
+        TABLE_PATH, encoding='unicode_escape', engine='python')
+    X = table[['long', 'lat']]
+    print(X['long'].min(), X['long'].max())
+
+    brc = Birch(threshold=0.01, n_clusters=None)
+    brc.fit(X.values)
+    clusters = brc.predict(X.values)
+    table['clusters'] = clusters
+
+    _generate_scatter_plot(clusters, table)
 
     name_to_edit_distance = collections.defaultdict(set)
     for cluster in numpy.unique(clusters):
