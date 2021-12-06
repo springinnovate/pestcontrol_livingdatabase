c873df40212f6db39f6b8ca730fd4120489aba6c
Rich Sharp <rich@therealspring.org>
2021-12-06 09:56:44 -0800

    complete with standard edit distance

--- clean_table.py
+++ clean_table.py
@@ -1,5 +1,6 @@
 """Scrub table fields."""
 import os
+import threading
 
 from matplotlib import colors
 from sklearn.cluster import Birch
@@ -27,7 +28,7 @@ def _generate_scatter_plot(clusters, table):
             f'{os.path.basename(TABLE_PATH)} {len(clusters)} clusters\n'
             f'within ${CLUSTER_RESOLUTION}^\\circ$ of each other')
     plt.savefig(
-        f'{os.path.basename(os.path.splitext(TABLE_PATH)[0])}.png', dpi=600)
+        f'{os.path.basename(os.path.splitext(TABLE_PATH)[0])}.png', dpi=300)
 
 
 def main():
@@ -55,13 +56,12 @@ def main():
          for b in unique_names])
          for a in unique_names]
 
-    print('generating edit distance')
-    print(name_to_edit_distance[next(iter(name_to_edit_distance))])
     for max_edit_distance in range(1, 5):
+        edit_distance_table_path = f'candidate_table_{max_edit_distance}.csv'
+        print(f'generating {edit_distance_table_path}')
         processed_set = set()
-        with open(
-                f'candidate_table_{max_edit_distance}.csv', 'w',
-                encoding="ISO-8859-1") as candidate_table:
+        with open(edit_distance_table_path, 'w', encoding="ISO-8859-1") as \
+                candidate_table:
             for base_name, edit_distance_set in name_to_edit_distance.items():
                 if base_name in processed_set:
                     continue
@@ -74,40 +74,6 @@ def main():
                         break
                     processed_set.add(name)
                     row += f',"{name}"'
-                candidate_table.write(f'{row}\n')
-
-    return
-
-
-    return
-
-    for max_edit_distance in range(1, 10):
-        print(f'max edit edit_distance {max_edit_distance}')
-        table = pandas.read_csv(
-            TABLE_PATH, encoding='unicode_escape', engine='python')
-        all_names_df = table['region'].dropna().apply(lambda x: x.lower())
-        names_by_count = all_names_df.value_counts()
-
-        name_set = set(names_by_count.index)
-        cross_product = {
-            a: sorted(
-                [(editdistance.eval(a, b), b) for b in name_set if a != b])
-            for a in name_set}
-
-        with open(f'candidate_table_{max_edit_distance}.csv', 'w', encoding="utf-8") as candidate_table:
-            for name, count in zip(names_by_count.index, names_by_count):
-                print(name, count)
-                if name not in name_set:
-                    continue
-                name_set.remove(name)
-                row = f'"{name}"'
-                for edit_distance, candidate in cross_product[name]:
-                    if candidate not in name_set:
-                        continue
-                    if edit_distance > max_edit_distance:
-                        break
-                    row += f',"{candidate}"'
-                    name_set.remove(candidate)
                 candidate_table.write(f'{row}\n')
 
 
