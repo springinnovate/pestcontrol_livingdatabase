473a9bfc84f1792eebc592208013a012d4cf897d
Rich Sharp <rich@therealspring.org>
2021-12-06 11:38:31 -0800

    adding cluster resolution size

--- clean_table.py
+++ clean_table.py
@@ -1,6 +1,6 @@
 """Scrub table fields."""
+import argparse
 import os
-import threading
 
 from matplotlib import colors
 from sklearn.cluster import Birch
@@ -10,11 +10,7 @@ import matplotlib.pyplot as plt
 import numpy
 import pandas
 
-TABLE_PATH = 'plotdata.cotton.select.csv'
-CLUSTER_RESOLUTION = 0.01
-
-
-def _generate_scatter_plot(clusters, table):
+def _generate_scatter_plot(table_path, cluster_resolution, clusters, table):
     """Generate scatter plot of clusters."""
     print('generating scatter plot')
     fig, ax = plt.subplots()
@@ -25,10 +21,10 @@ def _generate_scatter_plot(clusters, table):
             x='long', y='lat', ax=ax, s=0.01, marker='x',
             color=colorlist[i % len(colorlist)])
         ax.set_title(
-            f'{os.path.basename(TABLE_PATH)} {len(clusters)} clusters\n'
-            f'within ${CLUSTER_RESOLUTION}^\\circ$ of each other')
+            f'{os.path.basename(table_path)} {len(clusters)} clusters\n'
+            f'within ${cluster_resolution}^\\circ$ of each other')
     plt.savefig(
-        f'{os.path.basename(os.path.splitext(TABLE_PATH)[0])}.png', dpi=300)
+        f'{os.path.basename(os.path.splitext(table_path)[0])}.png', dpi=300)
 
 
 def _modified_edit_distance(a, b):
@@ -54,17 +50,26 @@ def _modified_edit_distance(a, b):
 
 def main():
     """Entry point."""
+    parser = argparse.ArgumentParser(
+        description='search for similar names in space and edit distance')
+    parser.add_argument('table_path', help=(
+        'path to CSV data table with "long", "lat" and "technician" fields'))
+    parser.add_argument(
+        '--cluster_resolution', required=True, type=float,
+        help='cluster distance size to group names to check for edit distance')
+    args = parser.parse_args()
     table = pandas.read_csv(
-        TABLE_PATH, encoding='unicode_escape', engine='python')
+        args.table_path, encoding='unicode_escape', engine='python')
     X = table[['long', 'lat']]
     print(X['long'].min(), X['long'].max())
 
-    brc = Birch(threshold=0.01, n_clusters=None)
+    brc = Birch(threshold=args.cluster_resolution, n_clusters=None)
     brc.fit(X.values)
     clusters = brc.predict(X.values)
     table['clusters'] = clusters
 
-    _generate_scatter_plot(clusters, table)
+    _generate_scatter_plot(
+        args.table_path, args.cluster_resolution, clusters, table)
 
     name_to_edit_distance = collections.defaultdict(set)
     for cluster in numpy.unique(clusters):
@@ -78,7 +83,9 @@ def main():
          for a in unique_names]
 
     for max_edit_distance in range(1, 8):
-        edit_distance_table_path = f'candidate_table_{max_edit_distance}.csv'
+        edit_distance_table_path = (
+            f'candidate_table_{max_edit_distance}_'
+            f'{args.cluster_resolution}.csv')
         print(f'generating {edit_distance_table_path}')
         processed_set = set()
         with open(edit_distance_table_path, 'w', encoding="ISO-8859-1") as \
