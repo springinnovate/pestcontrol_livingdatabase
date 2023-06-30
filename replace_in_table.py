a9a0a857bdd78c9b344cb23b9af27fea976e6057
Rich Sharp <richpsharp@gmail.com>
2023-06-30 10:56:45 -0700

    fixing issue where replacement didn't work

--- replace_in_table.py
+++ replace_in_table.py
@@ -45,37 +45,13 @@ def main():
         connected_components = nx.connected_components(graph)
         for edge_set in connected_components:
             match_list = list(sorted(edge_set, key=lambda x: len(x)))
+            LOGGER.debug(match_list)
             for element in match_list[1:]:
+                base_table[fieldname] = clean(base_table[fieldname])
                 base_table[fieldname] = base_table[fieldname].replace(element, match_list[0])
-    base_table.to_csv(f'fixed_{base_table_path}')
-
-    return
-    # replacement_table_path = base_table.iloc[0]
-    # LOGGER.debug(replacement_table_path)
-    # return
-
-    # Select only the string columns, then apply the function
-    base_table.loc[:, base_table.dtypes == object] = base_table.select_dtypes(
-        include=[object]).apply(clean)
-
-    print(base_table['technician'])
-    replacement_table = csv.reader(
-        clean_io(args.replacement_table_path))
-    for row in replacement_table:
-        print(row)
-        print(base_table[args.field_name])
-        if len(row) <= 1:
-            continue
-        base_table.replace(
-           {args.field_name: row[1:]}, row[0], inplace=True)
-
-    suffix = args.target_suffix
-    if suffix is None:
-        suffix = args.field_name
-    target_table_path = f'%s{suffix}%s' % os.path.splitext(
-        args.base_table_path)
-    base_table.to_csv(target_table_path, index=False, encoding='utf-8')
-    print(f'result written to {target_table_path}')
+    target_table_path = f'fixed_{os.path.basename(base_table_path)}'
+    LOGGER.info(f'fixed table at {target_table_path}')
+    base_table.to_csv(target_table_path)
 
 
 if __name__ == '__main__':
