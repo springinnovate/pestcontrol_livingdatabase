6985e1545ea1ac413f8634e96f0b522e4658e278
Rich Sharp <richpsharp@gmail.com>
2023-10-06 18:02:08 -0700

    roll it all in one batch

--- scrub_table.py
+++ scrub_table.py
@@ -5,7 +5,7 @@ import logging
 import os
 import sys
 
-from iterables import islice
+from itertools import islice
 from clean_table import _process_line
 
 logging.basicConfig(
@@ -36,6 +36,8 @@ def main():
     scrubbed_file = open(
         f'''scrubbed_{os.path.basename(os.path.splitext(
             args.table_path)[0])}.csv''', 'wb')
+    scrubbed_file.write(b'\xEF\xBB\xBF')
+    missing_letter_set = set()
     with concurrent.futures.ProcessPoolExecutor() as executor:
         print(f'process reading {args.table_path}')
         with open(args.table_path, 'rb') as table_file:
@@ -45,26 +47,17 @@ def main():
             processed_lines = []
             for lines_batch in batch(table_file, 1000):
                 n_lines -= 1000
-                processed_lines += list(
+                processed_lines = list(
                     executor.map(_process_line, lines_batch))
                 print(f'{n_lines} left to process')
-        last_percent = 0
-        print('write scrubbed table')
-        scrubbed_file.write(b'\xEF\xBB\xBF')
-        missing_letter_set = set()
-        for line_no, line in enumerate(processed_lines):
-            if line_no % 1000 == 0:
-                print(f'processing line no: {line_no}')
-            missing_letter_set.update([
-                word.replace('"', '')
-                for element in line.decode('utf-8').split(',') if '_' in element
-                for word in element.split(' ')
-                if '_' in word])
-            percent_complete = line_no/n_lines*100
-            if percent_complete-last_percent >= 1:
-                last_percent = percent_complete
-                LOGGER.info(f'{last_percent:5.2f}% complete processing of {args.table_path}')
-            scrubbed_file.write(line)
+
+            for line_no, line in enumerate(processed_lines):
+                missing_letter_set.update([
+                    word.replace('"', '')
+                    for element in line.decode('utf-8').split(',') if '_' in element
+                    for word in element.split(' ')
+                    if '_' in word])
+                scrubbed_file.write(line)
 
 
 if __name__ == '__main__':
