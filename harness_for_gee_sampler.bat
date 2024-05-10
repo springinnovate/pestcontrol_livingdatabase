ae7f06842c53f8c9468e4bf14c5074204e6b4ba5
Richard Sharp <richpsharp@gmail.com>
2024-05-09 18:40:18 -0700

    updating longs

--- harness_for_gee_sampler.bat
+++ harness_for_gee_sampler.bat
@@ -1,6 +1,6 @@
-python gee_point_sampler.py --point_table_path mia_data_requests/almond_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
-python gee_point_sampler.py --point_table_path mia_data_requests/citrus_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
-python gee_point_sampler.py --point_table_path mia_data_requests/cotton_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
-python gee_point_sampler.py --point_table_path mia_data_requests/rice_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
-python gee_point_sampler.py --point_table_path mia_data_requests/vineyards_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
-python gee_point_sampler.py --point_table_path mia_data_requests/wheat_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
+CALL python gee_point_sampler.py --point_table_path mia_data_requests/almond_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
+CALL python gee_point_sampler.py --point_table_path mia_data_requests/citrus_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
+CALL python gee_point_sampler.py --point_table_path mia_data_requests/cotton_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
+CALL python gee_point_sampler.py --point_table_path mia_data_requests/rice_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
+CALL python gee_point_sampler.py --point_table_path mia_data_requests/vineyards_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
+CALL python gee_point_sampler.py --point_table_path mia_data_requests/wheat_lat_longs.csv --dataset_table_path mia_data_requests/mia_data_request.csv
