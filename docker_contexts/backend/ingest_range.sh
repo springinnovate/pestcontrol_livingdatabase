293b358f183337b3cce107450cf5c9b101e87e4d
Richard Sharp <richpsharp@gmail.com>
2024-07-15 14:46:16 -0700

    update ingest range

--- docker_contexts/backend/ingest_range.sh
+++ docker_contexts/backend/ingest_range.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-lower_bound=$1
-upper_bound=$2
-step_size=$3
+lower_bound=0
+upper_bound=750007
+step_size=10000
 
 input_csv1="./base_data/BioControlDatabaseMetadata.csv"
 input_csv2="./base_data/abundance_template_rbound.csv"
 
-for ((i=lower_bound; i<upper_bound; i+=step_size)); do
-    next_bound=$((i + step_size))
-    echo python3 ingest_table.py "$input_csv1" "$input_csv2" --n_dataset_rows "$i" "$next_bound"
+current_start=$lower_bound
+
+while [ $current_start -lt $upper_bound ]; do
+    current_end=$((current_start + step_size))
+    if [ $current_end -gt $upper_bound ]; then
+        current_end=$upper_bound
+    fi
+
+    echo python3 ingest_table.py "$input_csv1" "$input_csv2" --n_dataset_rows "$current_start" "$current_end"
+
+    current_start=$((current_end + 1))
 done
